fb66b80bfe9edd3e7cdca71187debf22fefe4e48
nathayde@hotmail.com <nathayde@hotmail.com>
2021-06-01 23:20:08 -0300

    Good solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,47 +7,42 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import model.exceptoins.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) throws DomainException {
 
-		// teste
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		System.out.print("Room number: ");
-		int roomNumber = sc.nextInt();
-		System.out.print("Check-in date (dd/MM/yyyy) : ");
-		Date checkIn = sdf.parse(sc.next());
-		System.out.print("Check-out date (dd/MM/yyyy) : ");
-		Date checkOut = sdf.parse(sc.next());
-		if (!checkOut.after(checkIn)) {
-			System.out.print("Error reservation: Check-out date must be after Check-in date");
-		} else {
+		try {
+			System.out.print("Room number: ");
+			int roomNumber = sc.nextInt();
+			System.out.print("Check-in date (dd/MM/yyyy) : ");
+			Date checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out date (dd/MM/yyyy) : ");
+			Date checkOut = sdf.parse(sc.next());
 			Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
 			System.out.print("Reservation: " + reservation.toString());
 
 			System.out.println();
 			System.out.println("Enter data to update reservation: ");
-
+			
 			System.out.print("Check-in date (dd/MM/yyyy) : ");
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-out date (dd/MM/yyyy) : ");
 			checkOut = sdf.parse(sc.next());
-
-     		String error = reservation.updateDates(checkIn, checkOut);
-     		if (error != null) {
-     			System.out.print("Error in reservation: " + error);
-     		}
-     		else {
-     			System.out.print("Reservation: " + reservation.toString());
-			}
-
+			reservation.updateDates(checkIn, checkOut);
 		}
-
+		catch (ParseException e) {
+			System.out.print("Invalid date format");
+		}
+		
+//		catch (IllegalArgumentException e) {
+		catch (DomainException e) {
+			System.out.print("Error in reservation: " + e.getMessage());
+		}
 		sc.close();
-
 	}
-
 }
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptoins.DomainException;
+
 public class Reservation {
 
 	private Integer roomNumber;
@@ -11,7 +13,11 @@ public class Reservation {
 	private Date checkOut;
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
-	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException {
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after Check-in date");
+			
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -38,19 +44,19 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) throws DomainException {
 		
 		Date now = new Date();
 		
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return "Error in reservation: Reservation dates for update must be future dates";
+			throw new DomainException ("Error in reservation: Reservation dates for update must be future dates");
 		}
 		if (!checkOut.after(checkIn)) {
-			return "Error reservation: Check-out date must be after Check-in date";
+			throw new DomainException("Error reservation: Check-out date must be after Check-in date");
+//			throw new IllegalArgumentException("Error reservation: Check-out date must be after Check-in date");
 		}
 		this.checkIn = checkIn;
 	    this.checkOut = checkOut;
-	    return null;
 	}
 	
 	@Override
